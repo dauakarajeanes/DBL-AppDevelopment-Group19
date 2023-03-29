95d995c336d33716100d2b5200227d22605b907b
dauakarajeanes <d.d.karajeanes@student.tue.nl>
2023-03-29 19:28:15 +0200

    Login Page added

--- Terra/app/src/main/java/com/appdev/terra/UserLoginScreen.java
+++ Terra/app/src/main/java/com/appdev/terra/UserLoginScreen.java
@@ -2,26 +2,28 @@ package com.appdev.terra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SplashScreen extends AppCompatActivity {
+public class UserLoginScreen extends AppCompatActivity {
 
-    Button buttonCitizen;
+    Button loginButton;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_user_login_screen);
 
-        buttonCitizen = (Button) findViewById(R.id.button);
+        loginButton = (Button) findViewById(R.id.login_button);
 
-        buttonCitizen.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SplashScreen.this,UserLoginScreen.class);
+                Intent intent = new Intent(UserLoginScreen.this,HomeScreen.class);
                 startActivity(intent);
 
             }
